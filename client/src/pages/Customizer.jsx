cfda0169fd3c76a32675c822c27c7ea4c7c9e192
GodoyMS <youremail@domain.com>
2023-04-07 00:06:03 -0500

    added backend url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -64,7 +64,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://shirt-ai-generator-backend.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
