b7f55b1c9cedb59bd9000378ac72ff3c8835e52c
GodoyMS <youremail@domain.com>
2023-04-07 02:07:10 -0500

    changed to http

--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import dalleRoutes from './routes/dalle.routes.js';
-
+import http from 'http';
 import https from 'https';
 
 dotenv.config();
 //keeep server alive
 
 setInterval(() => {
-  https.get('https://shirt-ai-generator-backend.onrender.com/', (res) => {
+  http.get('http://shirt-ai-generator-backend.onrender.com/', (res) => {
     console.log(`Ping status: ${res.statusCode}`);
   }).on('error', (err) => {
     console.error(`Ping error: ${err.message}`);
   });
-}, 30000);
+}, 3000);
 
 
 const app = express();
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-const server = https.createServer(app);
+const server =  new http.createServer(app);
 
 
 server.listen(8080, () => console.log('Server has started on port 8080'))
